2682bd07270960f97576d9e71a30a7e2547b3dc6
Harsha817 <52495573+Harsha817@users.noreply.github.com>
2023-01-20 14:45:59 +0530

    Add files via upload

---
+++ src/test/java/com/as2/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as2.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
